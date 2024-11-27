75821d7fb6629a8e49c3aaa0046961f5e120e2fe
Webagility <no-reply@webagility.io>
2024-11-27 03:01:38 +0000

    [webagility] update

--- components/plasmic/copy_of_t_7/PlasmicGlobalContextsProvider.tsx
+++ components/plasmic/copy_of_t_7/PlasmicGlobalContextsProvider.tsx
@@ -14,6 +14,7 @@ export interface GlobalContextsProviderProps {
   commerceProviderComponentProps?: Partial<
     Omit<React.ComponentProps<typeof CommerceProviderComponent>, "children">
   >;
+
   embedCssProps?: Partial<
     Omit<React.ComponentProps<typeof EmbedCss>, "children">
   >;
